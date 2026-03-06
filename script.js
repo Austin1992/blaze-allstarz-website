63ad099098ad35f8979e159c6a144fceaa3ca2a1
Akojuru Anthony <montana@Akojurus-MacBook-Pro.local>
2026-03-05 19:44:59 -0800

    update

--- script.js
+++ script.js
@@ -652,3 +652,33 @@ document.addEventListener('DOMContentLoaded', function() {
 document.addEventListener('DOMContentLoaded', () => {
     startCountdown();
 });
+
+
+
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const currentTheme = localStorage.getItem('theme');
+const themeIcon = document.getElementById('theme-icon');
+
+// Check for saved user preference
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'light') {
+        toggleSwitch.checked = true;
+        themeIcon.innerText = '☀️';
+    }
+}
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+        themeIcon.innerText = '☀️';
+    } else {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+        themeIcon.innerText = '🌙';
+    }    
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
